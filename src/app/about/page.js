f7b44443da242bdf8ed77d4dc4a60828cf0f4a4d
johannes <johannes.muljana@ait.nsw.edu.au>
2023-11-02 15:30:21 +1100

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,18 @@
+// import 'bootstrap/dist/css/bootstrap.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+export default function About(props) {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>About</h1>
+                </Col>
+                <Col>
+                    <h1>Hey</h1>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
